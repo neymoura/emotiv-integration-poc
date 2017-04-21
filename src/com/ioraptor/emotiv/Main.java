9bf26d7891f3deea99296b930044bfcde2762431
Ney Moura <neymourac@gmail.com>
2017-04-21 14:52:47 -0300

    Average frequency bands example

--- src/com/ioraptor/emotiv/Main.java
+++ src/com/ioraptor/emotiv/Main.java
@@ -56,10 +56,7 @@ public class Main {
                 if (readyToCollect && onStateChanged)
                 {
 
-                    System.out.println("Blink: " + EmoState.INSTANCE.IS_FacialExpressionIsBlink(eState));
-//                    System.out.println("Stress: " + PerformanceMetrics.INSTANCE.IS_PerformanceMetricGetStressScore(eState));
-
-                    System.out.println("Focus :" + PerformanceMetrics.INSTANCE.IS_PerformanceMetricGetFocusScore(eState));
+                    read(userID);
 
                 }
 
@@ -72,6 +69,64 @@ public class Main {
 
         emotivDisconnect();
 
+    }
+
+    private static void read(IntByReference userID) {
+
+        DoubleByReference alpha     = new DoubleByReference(0);
+        DoubleByReference low_beta  = new DoubleByReference(0);
+        DoubleByReference high_beta = new DoubleByReference(0);
+        DoubleByReference gamma     = new DoubleByReference(0);
+        DoubleByReference theta     = new DoubleByReference(0);
+
+        int successfulReads = 0;
+
+        double alphaSum = 0.0;
+        double lowBetaSum = 0.0;
+        double highBetaSum = 0.0;
+        double gammaSum = 0.0;
+        double thetaSum = 0.0;
+
+        for(int i = 3 ; i < 17 ; i++){
+
+            int result = Edk.INSTANCE.IEE_GetAverageBandPowers(userID.getValue(), i, theta, alpha, low_beta, high_beta, gamma);
+
+            if(result == EdkErrorCode.EDK_OK.ToInt()){
+
+                alphaSum += alpha.getValue();
+                lowBetaSum += low_beta.getValue();
+                highBetaSum += high_beta.getValue();
+                gammaSum += gamma.getValue();
+                thetaSum += theta.getValue();
+
+                successfulReads++;
+
+            }
+
+        }
+
+        double alphaAvg = alphaSum/successfulReads;
+        double lowBetaAvg = lowBetaSum/successfulReads;
+        double highBetaAvg = highBetaSum/successfulReads;
+        double gammaAvg = gammaSum/successfulReads;
+        double thetaAvg = thetaSum/successfulReads;
+
+        System.out.println(
+                  zeroNaN(alphaAvg) + ","
+                + zeroNaN(lowBetaAvg) + ","
+                + zeroNaN(highBetaAvg) + ","
+                + zeroNaN(gammaAvg) + ","
+                + zeroNaN(thetaAvg)
+        );
+
+    }
+
+    private static double zeroNaN(double d){
+        if(Double.isNaN(d)){
+            return 0.0;
+        }else{
+            return d;
+        }
     }
 
     private static void emotivDisconnect() {
